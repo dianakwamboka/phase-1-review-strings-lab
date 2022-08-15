0500879884da1cb55fc396bf5c350f98bd57bb37
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-16 00:09:43 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+const welcomeMessage = "Welcome to Flatbook!";
+
+const WelcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWecomeMessage = WelcomeMessage.toUpperCase();
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
