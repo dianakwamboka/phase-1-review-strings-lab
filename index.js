72c3f0dbf71aead6c885e723fcd469543f9c5a0e
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-23 15:05:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Write your code in this file!
-const currentUser = "Grace Hopper";
-const welcomeMessage = "Welcome to Flatbook!";
-
-const WelcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWecomeMessage = WelcomeMessage.toUpperCase();
-currentUser.slice(0, 1);
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
 
